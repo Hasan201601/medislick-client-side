d043f5e9c23d01a2b800f488752375aa2cd160c0
Hasan201601 <hasanuzzaman201601@gmail.com>
2021-10-18 16:30:09 +0600

    first commit

---
+++ src/components/Shared/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="">
+            <hr />
+            This is Footer
+        </div>
+    );
+};
+
+export default Footer;
